e64a61b28a283eea28696582801a1fb8efb56b8c
Hyeonjun Park <phjppo0918@gmail.com>
2023-07-27 15:42:20 +0900

    fix : nginx-certbot 설정 수정

--- dev/init-letsencrypt.sh
+++ dev/init-letsencrypt.sh
@@ -3,9 +3,9 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(dev.partyrun.online www.dev.partyrun.online)
+domains=dev.partyrun.online
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="../certbot"
 email="phjppo0918@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
